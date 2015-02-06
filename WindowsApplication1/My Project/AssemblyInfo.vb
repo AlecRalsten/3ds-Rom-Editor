8ce4e0eee9f072293c630cdb3395bc119715e60e
Aliak <aliakr18@gmail.com>
2015-02-06 03:23:20 -0500

    Bug Fixes

--- WindowsApplication1/My Project/AssemblyInfo.vb
+++ WindowsApplication1/My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("")> 
 <Assembly: AssemblyProduct("3ds Rom Editor")> 
-<Assembly: AssemblyCopyright("Copyright ©  2014")> 
+<Assembly: AssemblyCopyright("Copyright © Aliak Ralsten 2015")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.3")> 
-<Assembly: AssemblyFileVersion("0.3")> 
+<Assembly: AssemblyVersion("1.1")> 
+<Assembly: AssemblyFileVersion("1.1")> 
